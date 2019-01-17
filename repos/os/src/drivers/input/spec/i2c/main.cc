d47c407290ec7e86423f9ce92ed2e6611749b4b3
Johannes Kliemann <jk@jkliemann.de>
2019-01-17 15:50:46 +0100

    use acpica and report rom

--- repos/os/src/drivers/input/spec/i2c/main.cc
+++ repos/os/src/drivers/input/spec/i2c/main.cc
@@ -1,100 +1,34 @@
 
 #include <base/component.h>
-#include <base/heap.h>
-
-#include <acpica/acpica.h>
-
-extern "C" {
-#include "acpi.h"
-#include "accommon.h"
-#include "acevents.h"
-#include "acnamesp.h"
-}
-
-extern "C"
-void AcpiOsPrintf (const char *fmt, ...){}
-
-extern "C"
-void AcpiOsVprintf (const char *fmt, va_list va){}
+#include <base/attached_rom_dataspace.h>
+#include <base/signal.h>
 
 namespace I2C_HID
 {
     struct Main;
 }
 
-ACPI_STATUS AcpiOsInstallInterruptHandler(UINT32 irq, ACPI_OSD_HANDLER handler,
-                                                  void *context)
-{
-    Genode::warning(__func__, " not implemented");
-    return AE_OK;
-}
-
 struct I2C_HID::Main
 {
     Genode::Env &_env;
-    Genode::Heap _heap;
+    Genode::Attached_rom_dataspace _acpi;
+    Genode::Signal_handler<Main> _acpi_sigh;
 
-    static ACPI_STATUS detect(ACPI_HANDLE hid, UINT32, void *context, void **return_bridge)
+    void detect()
     {
-        I2C_HID::Main *main = reinterpret_cast<I2C_HID::Main *>(context);
-        Genode::log("I2C HID device detected ", main);
-        return AE_OK;
+        Genode::log(__func__);
+        _acpi.update();
+        Genode::log(_acpi.xml());
     }
 
     Main(Genode::Env &env) :
         _env(env),
-        _heap(env.ram(), env.rm())
+        _acpi(env, "i2c_hid"),
+        _acpi_sigh(env.ep(), *this, &Main::detect)
     {
-        Acpica::init(_env, _heap,
-                Acpica::Wait_acpi_ready{true},
-                Acpica::Act_as_acpi_drv{true});
-
-        ACPI_STATUS status = AcpiInitializeSubsystem();
-
-        if (status != AE_OK) {
-            Genode::error("AcpiInitializeSubsystem failed, status=", status);
-            return;
-        }
-
-        status = AcpiInitializeTables(nullptr, 0, true);
-        if (status != AE_OK) {
-            Genode::error("AcpiInitializeTables failed, status=", status);
-            return;
-        }
-
-        status = AcpiLoadTables();
-        if (status != AE_OK) {
-            Genode::error("AcpiLoadTables failed, status=", status);
-            return;
-        }
-
-        status = AcpiEnableSubsystem(ACPI_FULL_INITIALIZATION);
-        if (status != AE_OK) {
-            Genode::error("AcpiEnableSubsystem failed, status=", status);
-            return;
-        }
-
-        status = AcpiInitializeObjects(ACPI_NO_DEVICE_INIT);
-        if (status != AE_OK) {
-            Genode::error("AcpiInitializeObjects (no devices) failed, status=", status);
-            return;
-        }
-
-        /* set APIC mode */
-        /*
-        status = init_pic_mode();
-        if (status != AE_OK) {
-            Genode::error("Setting PIC mode failed, status=", status);
-            return;
-        }
-        */
-
-        status = AcpiGetDevices(ACPI_STRING("PNP0C50"), Main::detect, this, nullptr);
-        if (status != AE_OK) {
-            Genode::error("AcpiGetDevices failed, status=", status);
-            return;
-        }
-
+        Genode::log("I2C HID device");
+        _acpi.update();
+        Genode::log(_acpi.xml());
     }
 };
 
--- repos/os/src/drivers/input/spec/i2c/target.mk
+++ repos/os/src/drivers/input/spec/i2c/target.mk
@@ -1,5 +1,5 @@
 TARGET = i2c_hid_drv
 REQUIRES = x86
 SRC_CC = main.cc
-LIBS = base acpica
+LIBS = base
 CC_CXX_WARN_STRICT :=
